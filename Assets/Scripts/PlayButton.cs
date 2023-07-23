0b367c1816c6e17145e87c8d59c56bd320741373
SnowyWalk <chunsa1166jp@gmail.com>
2023-07-24 06:05:55 +0900

    Add title and buttons. Add PLAY button can teleport everyone to the basement. Remove mirror's red post process effect. Reduce 30% of heartbeat volume.

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UdonSharpEditor;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayButton : UdonSharpBehaviour
+{
+    public Transform spawnPointToBasement;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(TeleportToBasement));
+    }
+
+    //[VRC.SDKBase.RPC]
+    public void TeleportToBasement()
+    {
+        Networking.LocalPlayer.TeleportTo(spawnPointToBasement.position, spawnPointToBasement.rotation);
+    }
+}
